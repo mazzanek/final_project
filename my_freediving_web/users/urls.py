7651bd711c16270d88151028311c85a31cf3d41e
mazzanek <158159078+mazzanek@users.noreply.github.com>
2024-09-02 15:07:00 +0200

    model complete

--- my_freediving_web/users/urls.py
+++ my_freediving_web/users/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    path('profile', profile, name="profile"),
+    path('accounts/profile/', profile, name="profile"),
     path('register/', register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
