cd269d550034112a7d39b9415d4d5db484a3a758
mazzanek <158159078+mazzanek@users.noreply.github.com>
2024-09-08 18:27:16 +0200

    final - oprava phyton anywhere

--- my_freediving_web/my_freediving_web/settings.py
+++ my_freediving_web/my_freediving_web/settings.py
@@ -118,10 +118,20 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
+"""
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+"""
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
